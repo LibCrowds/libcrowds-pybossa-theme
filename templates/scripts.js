2501d73133a007a88d74dce9095deabc946505f2
alexandermendes <alexanderhmendes@gmail.com>
2015-02-19 12:24:31 +0000

    Navbar transitions only occur on home page

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,9 +1,11 @@
 $(window).scroll(function() {
-    if($(this).scrollTop() > 100) {
-        $('.navbar').addClass('opaque');
-        $('.navbar-fixed-top').addClass('opaque');
-    } else {
-        $('.navbar').removeClass('opaque');
-        $('.navbar-fixed-top').removeClass('opaque');
+    if ($("#homepage-flag").length > 0) {
+        if($(this).scrollTop() > 100) {
+            $('.navbar').addClass('opaque');
+            $('.navbar-fixed-top').addClass('opaque');
+        } else {
+            $('.navbar').removeClass('opaque');
+            $('.navbar-fixed-top').removeClass('opaque');
+        }
     }
 });
