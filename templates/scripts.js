065b94d10e8a35d5bca42fda88fe31d8dba19f55
alexandermendes <alexanderhmendes@gmail.com>
2015-02-20 18:07:09 +0000

    The right navbar has been fixed

--- templates/scripts.js
+++ templates/scripts.js
@@ -2,6 +2,8 @@ $(function() {
     if ($("#homepage-flag").length < 1) {
         $('.navbar').addClass('opaque');
         $('.navbar-fixed-top').addClass('opaque');
+        $('.dropdown-menu').addClass('opaque');
+        $('.navbar-default').addClass('opaque');
     }
 });
 
@@ -10,9 +12,13 @@ $(window).scroll(function() {
         if($(this).scrollTop() > 100) {
             $('.navbar').addClass('opaque');
             $('.navbar-fixed-top').addClass('opaque');
+            $('.dropdown-menu').addClass('opaque');
+            $('.navbar-default').addClass('opaque');
         } else {
             $('.navbar').removeClass('opaque');
             $('.navbar-fixed-top').removeClass('opaque');
+            $('.dropdown-menu').removeClass('opaque');
+            $('.navbar-default').removeClass('opaque');
         }
     }
 });
