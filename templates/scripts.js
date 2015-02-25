d960727b708fd5e2ff0dc925a1da7a8c8236d511
alexandermendes <alexmendes@Alexs-MacBook-Pro.local>
2015-02-25 21:06:18 +0000

    Removed redundant javascript

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,13 +1,11 @@
 $(function() {
-    if ($("#homepage-flag").length < 1) {
-        $('.navbar').addClass('opaque');
-        $('.navbar-fixed-top').addClass('opaque');
-        $('.dropdown-menu').addClass('opaque');
-        $('.navbar-default').addClass('opaque');
+    if ($("#homepage-flag").length > 0) {
+        console.log('homepage recognised');
+        $('.navbar').removeClass('opaque');
+        $('.dropdown-menu').removeClass('opaque');
     }
     
     $('.edit-profile-image').on( "click", function() {
-        console.log('something clicked');
         $('#avatar').click();
     });
     
@@ -17,15 +15,11 @@ $(window).scroll(function() {
     if ($("#homepage-flag").length > 0) {
         if($(this).scrollTop() > 100) {
             $('.navbar').addClass('opaque');
-            $('.navbar-fixed-top').addClass('opaque');
             $('.dropdown-menu').addClass('opaque');
-            $('.navbar-default').addClass('opaque');
             $('#navbar-logo').attr('src', $('#logo-black').attr('data-src'));
         } else {
             $('.navbar').removeClass('opaque');
-            $('.navbar-fixed-top').removeClass('opaque');
             $('.dropdown-menu').removeClass('opaque');
-            $('.navbar-default').removeClass('opaque');
             $('#navbar-logo').attr('src', $('#logo-white').attr('data-src'));
         }
     }
