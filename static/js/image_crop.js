b057c9cb99dca60a3a2758a7d7f0f6ba2a294c62
alexandermendes <alexanderhmendes@gmail.com>
2015-03-20 16:14:53 +0000

    Updated project detail page and other small fixes

--- static/js/image_crop.js
+++ static/js/image_crop.js
@@ -43,6 +43,42 @@
         };
     }
 
+        function _previewImageSquare(){
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById("avatar").files[0]);
+        
+        oFReader.onload = function (oFREvent) {
+            document.getElementById("uploadPreview").src = oFREvent.target.result;
+            var img = document.getElementById('uploadPreview'); 
+            //or however you get a handle to the IMG
+            var width = img.clientWidth;
+            var height = img.clientHeight;
+            if (width > height) {
+                width = height - 100;
+            }
+            else {
+                height = width - 100;
+            }
+
+            jQuery(function($) {
+                $('#uploadPreview').Jcrop({
+                    onSelect:    _updateCoords,
+                    onChange:    _updateCoords,
+                    bgColor:     'black',
+                    bgOpacity:   .4,
+                    minSize: [100, 100],
+                    setSelect:   [  0, 
+                                    0, 
+                                    (width / 2), 
+                                    (height/ 2)],
+                    aspectRatio: 1,
+                    boxWidth: 450
+
+                });
+            });
+        };
+    }
+
     function _updateCoords(c){
         $("#x1").val(Math.floor(c.x));
         $("#y1").val(Math.floor(c.y));
@@ -65,6 +101,13 @@ $('#modal').on('shown.bs.modal', function (e) {
 });
 
 function loadPreview() {
+    if(typeof document.getElementById("avatar").files[0] != 'undefined') {
+        $("#loading").show();
+        previewImage();
+    }
+}
+
+function loadPreviewSquare() {
     if(typeof document.getElementById("avatar").files[0] != 'undefined') {
         $("#loading").show();
         previewImage();
