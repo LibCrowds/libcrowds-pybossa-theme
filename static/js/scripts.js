f8d18d4d89ec8d37e58fa9a5e0bc925e5450b18e
alexandermendes <alexanderhmendes@gmail.com>
2016-01-15 20:05:11 +0000

    Reorganise CSS

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,75 +10,48 @@ $(function() {
             "background-image": 'url(' + customSplash + ")"
         });
     }
-    
-    //Progress button
-    if($('.btn-circle-progress').length){
-        var progress = $('.btn-circle-progress').attr('data-progress');
-        if (progress >=100) {
-            fillArc(1);
-            fillArc(2);
-            fillArc(3);
-            fillArc(4);
-            $('.btn-circle-progress').removeAttr('href');
-            $('.btn-circle-progress p').html('<i class="fa fa-thumbs-up fa-4x"></i>');
-        } else if (progress>=75) {
-            fillArc(1);
-            fillArc(2);
-            fillArc(3);
-            partialFillArc(4, progress - 75);
-        } else if (progress>=50) {
-            fillArc(1);
-            fillArc(2);
-            partialFillArc(3, progress - 50);
-        } else if (progress>25) {
-            fillArc(1);
-            partialFillArc(2, progress - 25);
-        } else {
-            partialFillArc(1, progress);
-        }
-    }
-    
+
     updateXML();
-    
+
     $(".btn-album").each(function(){
         $(this).off('click').on('click', function(){
             document.getElementById('album_id').value = $(this).attr('id');
             document.location.href = "#album_id";
         });
     });
-    
+
     if ($(".set-main-bg").length > 0) {
         $('body').addClass($(".set-main-bg").attr('data-bg'));
     }
-    
+
     if ($('.CodeMirror-gutter').length) {
         $('#code-btns').attr('padding-left', $('.CodeMirror-gutter').attr('width'));
     }
-    
+
     $(".polaroids .polaroid:nth-child(even)").addClass("even");
     $("ul.polaroids div:nth-child(even) .polaroid").addClass("even");
-    
+
     $('.fa-download').each(function() {
             var bytes = $(this).attr('data-filesize');
             if (typeof bytes !== typeof undefined && bytes !== false) {
                 $(this).html("<small> (" + bytesToSize(bytes) + ")</small>");
             }
     });
-    
+
     if (history.length > 1) {
         $("#btnBack").show();
     }
     else {
         $("#btnHome").show();
     }
-    
+
     $('.tasks-remaining').each(function() {
         var tasks = $(this).attr('data-tasks');
         var progress = $(this).attr('data-progress');
         var remaining = Math.ceil((100 - progress) * .01 * tasks);
         $(this).html('Tasks Remaining: ' + remaining);
     });
-    
+
     //EpicEditor
     if (typeof(EpicEditor) !== "undefined") {
         var epicEditorTextArea = $('#epiceditor').data('textarea');
@@ -96,7 +69,7 @@ $(function() {
         var editor = new EpicEditor(opts);
         editor.load();
     }
-    
+
     animateProgress();
     sameSize();
     styleNavigation();
@@ -121,9 +94,9 @@ function getShareStats(){
         $.getJSON('https://graph.facebook.com/?id=' + $(this).data('url') + '&callback=?', function(json) {
             if (typeof json['shares'] != 'undefined') {
                 if (json['shares'] == 1) {
-                    facebook.attr('data-content', 'Shared once on Facebook');   
+                    facebook.attr('data-content', 'Shared once on Facebook');
                 } else {
-                    facebook.attr('data-content', 'Shared ' + json['shares'] + ' times on Facebook');   
+                    facebook.attr('data-content', 'Shared ' + json['shares'] + ' times on Facebook');
                 }
                 $('#total-share-count').attr('data-count', parseInt($('#total-share-count').attr('data-count')) + json['shares']);
             }
@@ -134,9 +107,9 @@ function getShareStats(){
         $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url=' + $(this).data('url') + '&callback=?', function(json) {
             if (json['count'] > 0) {
                 if (json['shares'] == 1) {
-                    twitter.attr('data-content', 'Shared once on Twitter');  
+                    twitter.attr('data-content', 'Shared once on Twitter');
                 } else {
-                    twitter.attr('data-content', 'Shared ' + json['count'] + ' times on Twitter');   
+                    twitter.attr('data-content', 'Shared ' + json['count'] + ' times on Twitter');
                 }
                 $('#total-share-count').attr('data-count', parseInt($('#total-share-count').attr('data-count')) + json['count']);
             }
@@ -147,9 +120,9 @@ function getShareStats(){
         $.getJSON('https://www.linkedin.com/countserv/count/share?url=' + $(this).data('url') + '&callback=?', function(json) {
             if (json['count'] > 0) {
                 if (json['shares'] == 1) {
-                    linkedin.attr('data-content', 'Shared once on LinkedIn');  
+                    linkedin.attr('data-content', 'Shared once on LinkedIn');
                 } else {
-                    linkedin.attr('data-content', 'Shared ' + json['count'] + ' times on LinkedIn');   
+                    linkedin.attr('data-content', 'Shared ' + json['count'] + ' times on LinkedIn');
                 }
                 $('#total-share-count').attr('data-count', parseInt($('#total-share-count').attr('data-count')) + json['count']);
             }
@@ -182,9 +155,9 @@ function getShareStats(){
                     count = r.result.metadata.globalCounts.count;
                     if (count > 0) {
                         if (count == 1) {
-                            googleplus.attr('data-content', 'Shared once on Google+');  
+                            googleplus.attr('data-content', 'Shared once on Google+');
                         } else {
-                            googleplus.attr('data-content', 'Shared ' + count + ' times on Google+');   
+                            googleplus.attr('data-content', 'Shared ' + count + ' times on Google+');
                         }
                         $('#total-share-count').attr('data-count', parseInt($('#total-share-count').attr('data-count')) + count);
                     }
@@ -256,34 +229,6 @@ function recordIP(){
     $.ajax('/blplugin/record_ip');
 }
 
-function fillArc(quarter) {
-    $('.arc-q' + quarter).addClass('full');
-}
-
-function partialFillArc(quarter, progress){
-    var skew =  (25 - progress) * 3.6;
-    var degrees = 90;
-    switch (quarter) {
-        case 2:
-            degrees = 180;
-            break;
-        case 3:
-            degrees = 270;
-            break;
-        case 4:
-            degrees = 360;
-            break;
-    }
-    $('.arc-q' + quarter).css({
-        '-moz-transform':'rotate(' + degrees + 'deg) skewX(' + skew + 'deg)',
-        '-webkit-transform':'rotate(' + degrees + 'deg) skewX(' + skew + 'deg)',
-        '-ms-transform':'rotate(' + degrees + 'deg) skewX(' + skew + 'deg)',
-        '-o-transform':'rotate(' + degrees + 'deg) skewX(' + skew + 'deg)',
-        'transform':'rotate(' + degrees + 'deg) skewX(' + skew + 'deg)'
-    });
-    $('.arc-q' + quarter).addClass('percent' + (25 - progress));
-}
-
 function styleNavigation() {
     if ($(".splash").length > 0 && $(window).width() > 991 && $(this).scrollTop() < 100) {
         $('.navbar').removeClass('opaque');
@@ -318,7 +263,7 @@ function sameSize() {
     } else {
         $(".row .same-size").height(m2 + 50);
     }
-    
+
     var m = 0;
     $(".well").height("");
     $(".well").each(function(i, el) {
@@ -358,7 +303,7 @@ $(".input-popover").popover({
 $('.edit-profile-image').on("click", function() {
     $('#avatar').click();
 });
-    
+
 $('.anchor-btn').click(function(evt) {
     evt.preventDefault();
     var anchor = $(this).attr('data-anchor');
