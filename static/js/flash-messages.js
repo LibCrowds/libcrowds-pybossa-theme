a25be41a2c3259a86bf5d031b7b201b35e0c1a8f
alexandermendes <alexanderhmendes@gmail.com>
2016-09-19 14:45:16 +0100

    Update flash messages

--- static/js/flash-messages.js
+++ static/js/flash-messages.js
@@ -4,6 +4,7 @@ function pybossaNotify(msg, category){
     }
     var html = "<div class='alert alert-" + category + " fade'>" +
                "<a class='close' data-dismiss='alert' href='#'>&times;</a>" +
+               msg + 
                "</div>";
     $('#alert-messages').append(html);
     window.setTimeout(function () {
