01bc17339f4fef500b2276f36f2c9e782afd0994
alexandermendes <alexanderhmendes@gmail.com>
2016-11-01 22:29:35 +0000

    Fix flash messages

--- static/js/flash-messages.js
+++ static/js/flash-messages.js
@@ -2,10 +2,9 @@ function pybossaNotify(msg, category, fadeOut=false){
     if (category === undefined) {
         category = "info";
     }
-    var html = $("<div class='alert alert-" + category + " fade'>" +
-               "<a class='close' data-dismiss='alert' href='#'>&times;</a>" +
-               msg +
-               "</div>");
+    var html = $(`<div class="alert" alert-${category} fade">` 
+                 + `<a class="close" data-dismiss="alert" href="#">&times;</a>${msg}` 
+                 + `</div>`);
     $('#alert-messages').append(html);
 
     window.setTimeout(function () {
